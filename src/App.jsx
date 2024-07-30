3bbb296a7d943383fd100959485adfabc2a21be6
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-07-30 19:35:06 +0530

    update 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import FileDisplay from "./components/FileDisplay"
 import { useState } from "react";
 
 function App() {
+
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
 
   const isAudioAvailable = file || audioStream;
+
+  const handleAudioReset = () => {
+    setFile(null);
+    setAudioStream(null);
+  }
 
   return (
     <div className="flex flex-col max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
         {isAudioAvailable ? (
-          <FileDisplay />
+          <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={audioStream} />
         ) : (<HomePage setFile={setFile} setAudioStream={setAudioStream} />)}
       </section>
       <footer></footer>
